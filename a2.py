cc1e604487f2f39cd0c9329fdf49a29a3bb00a09
dilara2016 <m.123.hunain@gmail.com>
2025-06-08 12:18:54 +0200

    a2

---
+++ a2.py
@@ -0,0 +1,16 @@
+def isSubstring(s1,s2):
+    if s1 in s2:
+        return s2.index(s1)
+    return -1
+
+if __name__ == "__main__":
+    s1 = "welcome"
+    s2 = "welcome to codingal"
+    result = isSubstring(s1,s2)
+    if result == -1:
+        print("Not Present")
+    else:
+        print("Present at index " + str(result))
+
+
+        
